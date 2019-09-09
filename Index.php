0f15231ce66154a09ab3a3267eb979c5b6eec282
Axsiow <Axsiow@gmail.com>
2019-09-09 14:54:37 +0200

    Summary

--- Index.php
+++ Index.php
@@ -3,22 +3,42 @@
     <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://fonts.googleapis.com/css?family=Livvic|Ubuntu&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="style.css">
+	<title>BDE EPSI</title>
     </head>
     <header>
-        <ul>
-            <li><a href="#"></a>Accueil</li>
-            <li><a href="#"></a>Calendrier</li>
-            <li><a href="#"></a>Evénements proposé</li>
-            <li><a href="#"></a>Suggestion d'évenements</li>
-        </ul>
+        <div class="menu-toggler">
+            <div class="bar half start"></div>
+            <div class="bar"></div>
+            <div class="bar half end"></div>
+        </div>
+        <nav class="top-nav">
+            <ul class="nav-list">
+                <li><a href="index.html" class="nav-link">Accueil</a></li>
+                <li><a href="#about" class="nav-link">A propos</a></li>
+                <li><a href="#services" class="nav-link">Compétences</a> </li>
+                <li><a href="#portfolio" class="nav-link">Portfolio</a> </li>
+                <li><a href="#experience" class="nav-link">Expérience</a> </li>
+                <li> <a href="#contact" class="nav-link">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="landing-text">
+            <h1>Charles Poncy Calixte</h1>
+            <h6>Etudiant en informatique</h6>
+        </div>
     </header>
+
+ 
+	<section class="sectionCenter">
+		<p>Test</p>
+	</section>
     <footer id="footer">
         <main>
             <p class="pfooter">Créé avec ❤️️ à Grenoble, France 🗻</p>
-            <p class="pfooter2"></p>
             <p class="pfooter">© 2019 BDE EPSI, Tous droits réservés.</p>
         </main>
     </footer>
+    <script src="scriptnavbar.js"></script>
 </html>
