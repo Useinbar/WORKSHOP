88ce6fb9b5c6b543f6101287eab4e0ba218a8440
Poncy <c.poncycalixte@epsi.fr>
2019-09-09 13:23:53 +0200

    en-tete

---
+++ Suggestion.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    </head>
+</html>
