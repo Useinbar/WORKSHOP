558d8c4aab92ba3c826ba131ee8a1bcdaec9a0aa
Axsiow <Axsiow@gmail.com>
2019-09-09 13:32:17 +0200

    Summary

--- Index.php
+++ Index.php
@@ -1,9 +1,12 @@
-<!doctype html>
-<html lang="fr">
-    <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    </head>
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>BDE EPSI</title>
+</head>
+<body>
+	
+</body>
 </html>
