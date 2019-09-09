49a914344c734cbff347723e105a1f684429ed80
Poncy <c.poncycalixte@epsi.fr>
2019-09-09 13:39:36 +0200

    noir et css

--- Evenement.php
+++ Evenement.php
@@ -6,4 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
     </head>
+    <header>
+        <ul>    
+            <li><a href="#"></a>Accueil</li>
+            <li><a href="#"></a>Calendrier</li>
+            <li><a href="#"></a>Evénements proposé</li>
+            <li><a href="#"></a>Suggestion d'évenements</li>
+        </ul>
+    </header>
 </html>
