f7aeb841a879d374721c189f1ad2b6402b127a9c
Axsiow <Axsiow@gmail.com>
2019-09-09 15:04:12 +0200

    Summary

--- Index.php
+++ Index.php
@@ -3,7 +3,8 @@
     <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 	<link href="https://fonts.googleapis.com/css?family=Livvic|Ubuntu&display=swap" rel="stylesheet"> 
 	<link rel="stylesheet" href="style.css">
 	<title>BDE EPSI</title>
@@ -40,5 +41,7 @@
             <p class="pfooter">© 2019 BDE EPSI, Tous droits réservés.</p>
         </main>
     </footer>
-    <script src="scriptnavbar.js"></script>
+    <script src="main.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
 </html>
